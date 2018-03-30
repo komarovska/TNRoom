a298f13ea9e468c15f645f7ebffe7dcea2fc826d
emiroshko <miro6ko@gmail.com>
2018-03-30 21:41:02 +0300

    adaptive

---
+++ docs/app.bundle.4b010495bfdbac046976.js
@@ -0,0 +1,33 @@
+webpackJsonp([0],[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(6);
+module.exports = __webpack_require__(7);
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+(() => {
+
+    // $('body').append('<div>jQuery works!:)</div>');
+
+
+})();
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@media (max-width: 990px) {\n^\n      Media query expression must begin with '('\n      in /Users/janemiroshko/Documents/projects/thenailsroom/thnr/src/sass/app/main-page-mobile.scss (line 22, column 1)\n    at runLoaders (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/janemiroshko/Documents/projects/thenailsroom/thnr/node_modules/node-sass/lib/index.js:294:32)");
+
+/***/ })
+],[5]);
