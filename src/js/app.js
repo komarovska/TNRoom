d80af3db6f1961713966fb35f3e737c2f519c2be
Victoria Komarovska <38187133+komarovska@users.noreply.github.com>
2018-05-07 23:25:45 +0300

    added one module

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 const Header = require('./modules/header');
 const Slick = require('./modules/slick');
+const Test = require('./modules/test');
 
 class App {
     init() {
         this.headerModule = new Header().init();
         this.slickModule = new Slick().init();
+        this.testModule = new Test().init();
     }
 }
 
