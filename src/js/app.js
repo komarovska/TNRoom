5989cc970703d21fdf6ea79a1813b87737951f1a
Victoria Komarovska <38187133+komarovska@users.noreply.github.com>
2018-05-08 16:09:09 +0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 const Header = require('./modules/header');
 const Slick = require('./modules/slick');
-const Test = require('./modules/scroll');
+const Scroll = require('./modules/scroll');
+const Carousel = require('./modules/carousel');
 
 class App {
     init() {
         this.headerModule = new Header().init();
         this.slickModule = new Slick().init();
         this.scrollModule = new Scroll().init();
+        this.carouselModule = new Carousel().init();
     }
 }
 
